0d57065bc20488b4b6afea55efc72f2a27027138
BT_1N3_24 <sample@bths.edu>
2023-09-28 10:52:33 -0400

    added point class and runner

---
+++ src/Point.java
@@ -0,0 +1,57 @@
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point(int x) {
+        this.x = x;
+        this.y = x;
+    }
+
+    public Point() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String coordinate() {
+        return "(" + x + "," + y + ")";
+    }
+
+    public String quadrant() {
+        if (x == 0 && y == 0) {
+            return "origin";
+        } else if (x == 0 || y == 0) {
+            return "on an axis";
+        } else if (x > 0 && y > 0) {
+            return "I";
+        } else if (x < 0 && y > 0) {
+            return "II";
+        } else if (x < 0 && y < 0) {
+            return "III";
+        } else if (x > 0 && y < 0) {
+            return "IV";
+        } else {
+            return null;
+        }
+    }
+}
